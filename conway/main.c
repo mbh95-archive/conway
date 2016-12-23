24c03d32c300ebd2461971cb94850f5288299771
matthewhammond <mbh2@rice.edu>
2016-12-23 01:09:25 -0600

    Initial Commit

---
+++ conway/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  conway
+//
+//  Created by Matthew Hammond on 12/23/16.
+//  Copyright © 2016 Matthew Hammond. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
